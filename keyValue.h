29a2e4b3e040cfe6bfaa80d01411c0dc4921bcf3
Zeyi Wen@ss8 <wenzeyi@gmail.com>
2016-03-10 16:46:41 +1100

    added keyValue.h file

---
+++ keyValue.h
@@ -0,0 +1,19 @@
+/*
+ * keyValue.h
+ *
+ *  Created on: 9 Mar 2016
+ *      Author: Zeyi Wen
+ *		@brief: 
+ */
+
+#ifndef KEYVALUE_H_
+#define KEYVALUE_H_
+
+struct key_value{
+	double featureValue;
+	int id;//This id may be used as feature id or instance id
+};
+
+
+
+#endif /* KEYVALUE_H_ */
