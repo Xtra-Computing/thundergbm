b230232cafb5c0f76539f5c2e5b390e8b5a35bc9
Zeyi Wen <wenzeyi@gmail.com>
2019-08-09 12:59:00 +0800

    update setup.py

--- python/setup.py
+++ python/setup.py
@@ -17,12 +17,16 @@ else:
 if not path.exists(path.join(dirname, "thundergbm", path.basename(lib_path))):
     copyfile(lib_path, path.join(dirname, "thundergbm", path.basename(lib_path)))
 setuptools.setup(name="thundergbm",
-                 version="0.3.4",
+                 version="0.3.7",
                  packages=["thundergbm"],
                  package_dir={"python": "thundergbm"},
                  description="A Fast GBM Library on GPUs and CPUs",
                  long_description="""The mission of ThunderGBM is to help users easily and efficiently apply GBDTs and Random Forests to solve problems. ThunderGBM exploits GPUs and multi-core CPUs to achieve high efficiency""",
                  long_description_content_type="text/plain",
+                 license='Apache-2.0',
+                 author='Xtra Computing Group',
+                 maintainer='thundergbm contributors',
+                 maintainer_email='wenzy@comp.nus.edu.sg',
                  url="https://github.com/zeyiwen/thundergbm",
                  package_data={"thundergbm": [path.basename(lib_path)]},
                  install_requires=['numpy', 'scipy', 'scikit-learn'],
